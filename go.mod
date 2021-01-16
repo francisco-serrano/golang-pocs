d44143cf0c0e42f6b5304979ce7943d8f14b952c
francisco-serrano <francisco.serrano372@gmail.com>
2021-01-16 16:06:20 -0300

    adds oauth2 stuff

--- go.mod
+++ go.mod
@@ -3,17 +3,16 @@ module github.com/francisco-serrano/awesomeProject
 go 1.13
 
 require (
-	github.com/c9s/goprocinfo v0.0.0-20200311234719-5750cbd54a3b
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-kit/kit v0.10.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/ilyakaznacheev/cleanenv v1.2.1
 	github.com/jinzhu/gorm v1.9.12
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/shirou/gopsutil v2.20.4+incompatible
 	github.com/sirupsen/logrus v1.4.2
 	github.com/t-tomalak/logrus-easy-formatter v0.0.0-20190827215021-c074f06c5816
-	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
-	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/oauth2 v0.0.0-20210113205817-d3ed898aa8a3
 )
---
+++ oauth/main/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var githubConnections map[string]string
+
+type githubResponse struct {
+	Data struct {
+		Viewer struct {
+			ID string `json:"id"`
+		} `json:"viewer"`
+	} `json:"data"`
+}
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "670b383c12b8bcc3fb01",
+	ClientSecret: "36a64ec1ac39211bb33cf8bb9949dc321c520218",
+	Endpoint:     github.Endpoint,
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/oauth/github", startGithubOauth)
+	http.HandleFunc("/oauth2/receive", completeGithubOauth)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	file, err := ioutil.ReadFile("./oauth/main/sample.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := fmt.Fprint(w, string(file)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startGithubOauth(w http.ResponseWriter, r *http.Request) {
+	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func completeGithubOauth(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	state := r.FormValue("state")
+
+	if state != "0000" {
+		http.Error(w, "state is incorrect", http.StatusBadRequest)
+		return
+	}
+
+	token, err := githubOauthConfig.Exchange(r.Context(), code)
+	if err != nil {
+		http.Error(w, "could not login", http.StatusInternalServerError)
+		return
+	}
+
+	ts := githubOauthConfig.TokenSource(r.Context(), token)
+
+	requestBody := strings.NewReader(`{"query": "query {viewer {id}}"}`)
+
+	client := oauth2.NewClient(r.Context(), ts)
+	resp, err := client.Post("https://api.github.com/graphql", "application/json", requestBody)
+	if err != nil {
+		http.Error(w, "could not get user", http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var githubResponse githubResponse
+	if err := json.NewDecoder(resp.Body).Decode(&githubResponse); err != nil {
+		http.Error(w, "github invalid response", http.StatusInternalServerError)
+		return
+	}
+
+	githubID := githubResponse.Data.Viewer.ID
+
+	userID, ok := githubConnections[githubID]
+	if !ok {
+		// new user, create account
+	}
+
+	// login to account userID using JWT
+}
