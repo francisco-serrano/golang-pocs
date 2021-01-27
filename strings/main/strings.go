937863f4408909dedca8e8b0f284450e121ef0c4
francisco-serrano <francisco.serrano372@gmail.com>
2021-01-26 21:03:31 -0300

    adfasfasd

--- strings/main/strings.go
+++ strings/main/strings.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
-func main() {
+func exampleA() {
 	const sLiteral = "\x99\x42\x32\x55\x50\x35\x23\x50\x29\x9c"
 	fmt.Println(sLiteral)
 	fmt.Printf("x: %x\n", sLiteral)
@@ -19,4 +24,43 @@ func main() {
 	fmt.Printf(" x: % x\n", sLiteral)
 
 	fmt.Printf("s: As a string: %s\n", sLiteral)
+}
+
+func exampleB() {
+	r := strings.NewReader("test")
+
+	fmt.Println("r length:", r.Len())
+
+	b := make([]byte, 1)
+
+	for {
+		n, err := r.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Println(err)
+			continue
+		}
+
+		fmt.Printf("read %s bytes: %d\n", b, n)
+	}
+
+	s := strings.NewReader("This is an error!\n")
+
+	fmt.Println("r length: ", s.Len())
+
+	n, err := s.WriteTo(os.Stderr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("wrote %d bytes to os.stderr\n", n)
+}
+
+func main() {
+	//exampleA()
+	exampleB()
 }
