1bbcdc78d46cfd4b375af64578ecf959bed0fabe
francisco-serrano <francisco.serrano372@gmail.com>
2021-01-30 23:03:39 -0300

    adfasdf

--- benchmark/main/benchmark.go
+++ benchmark/main/benchmark.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
 
 func fib1(n int) int {
 	if n == 0 {
@@ -44,6 +48,82 @@ func exampleA() {
 	fmt.Println(fib3(40))
 }
 
+var (
+	bufferSize int
+	fileSize   int
+)
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func createBuffer(buf *[]byte, count int) {
+	*buf = make([]byte, count)
+	if count == 0 {
+		return
+	}
+
+	for i := 0; i < count; i++ {
+		intByte := byte(random(0, 100))
+		if len(*buf) > count {
+			return
+		}
+
+		*buf = append(*buf, intByte)
+	}
+}
+
+func Create(dst string, b, f int) error {
+	if _, err := os.Stat(dst); err != nil {
+		return fmt.Errorf("File %s already exists.", dst)
+	}
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := destination.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	buf := make([]byte, 0)
+	for {
+		createBuffer(&buf, b)
+		buf = buf[:b]
+
+		if _, err := destination.Write(buf); err != nil {
+			return err
+		}
+
+		if f < 0 {
+			break
+		}
+
+		f -= len(buf)
+	}
+
+	return nil
+}
+
+func exampleB() {
+	output := "/tmp/randomFile"
+
+	bufferSize = 10
+	fileSize = 100000
+
+	if err := Create(output, bufferSize, fileSize); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := os.Remove(output); err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	exampleA()
+	//exampleA()
+	exampleB()
 }
